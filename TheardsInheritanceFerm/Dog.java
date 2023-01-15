ab8d85b0eb52bc875fee55e7714b6f203042bfb8
OlegBrozhyna <122048830+OlegBrozhyna@users.noreply.github.com>
2023-01-15 22:00:12 +0200

    Update Dog.java

--- TheardsInheritanceFerm/Dog.java
+++ TheardsInheritanceFerm/Dog.java
@@ -2,6 +2,7 @@ package OOPTheardsInheritanceTestingFerm;
 
 public class Dog extends Ferm{
     public Dog(String eat, String name, String locations, int size, int age) {
+        
         super(eat, name, locations, size, age);
     }
 }
