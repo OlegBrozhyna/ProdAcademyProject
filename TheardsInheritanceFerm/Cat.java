bc50769e4ca1c70313810169668399dd85931081
OlegBrozhyna <122048830+OlegBrozhyna@users.noreply.github.com>
2023-01-15 22:01:59 +0200

    Update Cat.java

--- TheardsInheritanceFerm/Cat.java
+++ TheardsInheritanceFerm/Cat.java
@@ -1,5 +1,7 @@
 package OOPTheardsInheritanceTestingFerm;
 
-
-public class Cat {
+public class Cat extends Ferm {
+  public Cat(String eat, String name, String locations, int size, int age) {
+        super(eat, name, locations, size, age);
+    }
 }
